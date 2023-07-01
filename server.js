ac0dd47102f3534c1b3c6a71f6a7a13fa2cc6ab8
TasnimMustafa <tasnimelshamia@gmail.com>
2023-07-01 02:21:00 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const express=require('express')
+const cors=require('cors')
+const app=express()
+const dotenv=require('dotenv')
+const helmet =require("helmet")
+const mongoose=require('mongoose')
+const cookieParser=require("cookie-parser")
+dotenv.config()
+const courseRouter=require('./routes/course.route')
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json())
+app.use(cookieParser());
+const url=process.env.DB_URL
+
+app.use('/course',courseRouter)
+// app.use('/course/img',express.static('uploads/courses'))
+
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+ }).then(()=>{
+     console.log("Database connected !!!!");
+ }).catch((err)=>{
+     console.log(err);
+     console.log("Database NOT CONNECTED");
+ })
+
+const PORT=process.env.PORT || 3000
+app.listen(PORT , ()=>{console.log(`App is running on port ${PORT}`);})
